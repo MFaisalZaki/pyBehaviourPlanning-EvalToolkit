6e93e0dade623b8981a7c89ccb1563961d7f5e34
MFaisalZaki <mustafa.faisal.eng@gmail.com>
2024-06-01 12:39:39 +0100

    adding fbi-different-encodings.

--- scripts/generate-all-exps-solve.sh
+++ scripts/generate-all-exps-solve.sh
@@ -2,6 +2,7 @@
 
 python3.11 exp-runner/initenv.py
 
-./scripts/generate-solve-exp.sh $(pwd)/all-exps/exp-fbi-behaviour-count $(pwd)/sandbox-fbi-behaviour-count
-./scripts/generate-solve-exp.sh $(pwd)/all-exps/exp-fi-behaviour-count $(pwd)/sandbox-fi-behaviour-count
-./scripts/generate-solve-exp.sh $(pwd)/all-exps/exp-symk-behaviour-count $(pwd)/sandbox-symk-behaviour-count
+./scripts/generate-solve-exp.sh $(pwd)/all-exps/exp-fbi-behaviour-count     $(pwd)/sandbox-fbi-behaviour-count
+./scripts/generate-solve-exp.sh $(pwd)/all-exps/exp-fi-behaviour-count      $(pwd)/sandbox-fi-behaviour-count
+./scripts/generate-solve-exp.sh $(pwd)/all-exps/exp-symk-behaviour-count    $(pwd)/sandbox-symk-behaviour-count
+./scripts/generate-solve-exp.sh $(pwd)/all-exps/exp-fbi-different-encodings $(pwd)/sandbox-fbi-different-encodings
