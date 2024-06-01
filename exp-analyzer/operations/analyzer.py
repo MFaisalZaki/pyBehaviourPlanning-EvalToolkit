7ca9e465526ef38b8edfb6f3f89bdd9661cda3e3
MFaisalZaki <mustafa.faisal.eng@gmail.com>
2024-06-01 23:55:43 +0100

    adding an initial code to plot the sat time for different encodings per problem.

--- exp-analyzer/operations/analyzer.py
+++ exp-analyzer/operations/analyzer.py
@@ -1,46 +1,16 @@
 import os
-import json
 
 from collections import defaultdict
+from collections import Counter
+
 
 
 from .utilities import (
-    getkeyvalue
+    read_files,
+    dump_list_to_csv
 )
+
 # We need a function to read experiments file and group them by planner, domain, problem
-
-def read_files(exp_dir):
-    ret_results = defaultdict(dict)
-    # Get all the files in the directory
-    for file in os.listdir(exp_dir):
-        if file.endswith(".json"):
-            with open(os.path.join(exp_dir, file), "r") as f:
-                data = json.load(f)
-                
-                # Read the planning task.
-                domain = getkeyvalue(data, "domain")
-                problem = getkeyvalue(data, "problem")                
-                if not domain in ret_results: ret_results[domain] = defaultdict(dict)
-                if not problem in ret_results[domain]: ret_results[domain][problem] = defaultdict(dict)
-
-                # read the required number of plans.
-                k = getkeyvalue(data, "k")
-                if not k in ret_results[domain][problem]: ret_results[domain][problem][k] = defaultdict(dict)
-
-                # read the quality bound.
-                q = getkeyvalue(data, "q")
-                if not q in ret_results[domain][problem][k]: ret_results[domain][problem][k][q] = defaultdict(dict)
-                
-                # read the planner.
-                tag = getkeyvalue(data, "tag")
-                if not tag in ret_results[domain][problem]: ret_results[domain][problem][k][q][tag] = defaultdict(dict)
-
-                # read the sat-time
-                ret_results[domain][problem][k][q][tag]["sat-time"] = getkeyvalue(data, "sat-time")
-
-    return ret_results
-
-
 def count_solved_instances(results):
     counter_values = defaultdict(dict)
     for domain, domain_results in results.items():
@@ -65,17 +35,51 @@ def count_solved_instances(results):
         for k, k_results in q_results.items():
             for tag, tag_results in k_results.items():
                 csv_dump.append(f'{q}, {k}, {tag}, {tag_results}')
-    # sort based on the k then tag.
+    # sort based on the q, k then tag.
     csv_dump = sorted(csv_dump, key=lambda x: (float(x.split(",")[0]), int(x.split(",")[1]), x.split(",")[2]))
     csv_dump = ['q, k, tag, solved_instances'] + csv_dump
     return csv_dump
 
-def dump_list_to_csv(csv_dump, output_file):
-    os.makedirs(os.path.dirname(output_file), exist_ok=True)
-    with open(output_file, "w") as f:
-        for line in csv_dump:
-            f.write(line + "\n")
-    return 0
+def analyze_sat_time(results):
+    def _remove_entries(dict_sat, _to_remove_keys):
+        for domain, problem, q, k in _to_remove_keys:
+            if domain in dict_sat and problem in dict_sat[domain] and q in dict_sat[domain][problem] and k in dict_sat[domain][problem][q]:
+                del dict_sat[domain][problem][q][k]
+            if domain in dict_sat and problem in dict_sat[domain] and q in dict_sat[domain][problem] and len(dict_sat[domain][problem][q]) == 0:
+                del dict_sat[domain][problem][q]
+            if domain in dict_sat and problem in dict_sat[domain] and len(dict_sat[domain][problem]) == 0:
+                del dict_sat[domain][problem]
+            if domain in dict_sat and len(dict_sat[domain]) == 0:
+                del dict_sat[domain]
+
+    planners_tags = set()
+    domain_sat_time_results = defaultdict(dict)
+    for domain, domain_results in results.items():
+        for problem, problem_results in domain_results.items():
+            for k, k_results in problem_results.items():
+                for q, q_results in k_results.items():
+                    for tag, tag_results in q_results.items():
+                        if not domain in domain_sat_time_results: domain_sat_time_results[domain] = defaultdict(dict)
+                        if not problem in domain_sat_time_results[domain]: domain_sat_time_results[domain][problem] = defaultdict(dict)
+                        if not q in domain_sat_time_results[domain][problem]: domain_sat_time_results[domain][problem][q] = defaultdict(dict)
+                        if not k in domain_sat_time_results[domain][problem][q]: domain_sat_time_results[domain][problem][q][k] = defaultdict(dict)
+                        if not tag in domain_sat_time_results[domain][problem][q][k]: domain_sat_time_results[domain][problem][q][k][tag] = []
+                        # ignore the timeout instances,. timeout is 600.0 seconds, we may want to have this as a parameter.
+                        timeout_instance = list(map(lambda x: eval(x.split(',')[1]) == 600.0, tag_results["sat-time"]))
+                        if any(timeout_instance) > 0: continue
+                        domain_sat_time_results[domain][problem][q][k][tag].append(tag_results["sat-time"])
+                        planners_tags.add(tag)
+
+    # now select the problem instances that are solved by all the planners.
+    solved_instances_by_all_planners = []
+    for domain, domain_results in domain_sat_time_results.items():
+        for problem, problem_results in domain_results.items():
+            for q, q_results in problem_results.items():
+                for k, k_results in q_results.items():
+                    for tag, tag_results in k_results.items():
+                        solved_instances_by_all_planners.append((domain, problem, q, k))
+    solved_instances_by_all_planners = list(filter(lambda item: item[1] > 2, Counter(solved_instances_by_all_planners).items()))
+    pass
 
 
 def analyze_different_encodings(args):
@@ -85,6 +89,8 @@ def analyze_different_encodings(args):
     solved_instaces_count = count_solved_instances(planners_results)
     # dump the results to a CSV file.
     dump_list_to_csv(solved_instaces_count, os.path.join(args.output_dir, "solved_instances.csv"))
+    # analyze the sat-time for each planner per domain.
+    sat_time_analysis = analyze_sat_time(planners_results)
     pass
 
 
--- exp-analyzer/operations/utilities.py
+++ exp-analyzer/operations/utilities.py
@@ -1,3 +1,7 @@
+import os
+import json
+from collections import defaultdict
+
 
 def getkeyvalue(data, target_key):
     if isinstance(data, dict):
@@ -13,3 +17,41 @@ def getkeyvalue(data, target_key):
             if result is not None:
                 return result
     return None
+
+def read_files(exp_dir):
+    ret_results = defaultdict(dict)
+    # Get all the files in the directory
+    for file in os.listdir(exp_dir):
+        if file.endswith(".json"):
+            with open(os.path.join(exp_dir, file), "r") as f:
+                data = json.load(f)
+                
+                # Read the planning task.
+                domain = getkeyvalue(data, "domain")
+                problem = getkeyvalue(data, "problem")                
+                if not domain in ret_results: ret_results[domain] = defaultdict(dict)
+                if not problem in ret_results[domain]: ret_results[domain][problem] = defaultdict(dict)
+
+                # read the required number of plans.
+                k = getkeyvalue(data, "k")
+                if not k in ret_results[domain][problem]: ret_results[domain][problem][k] = defaultdict(dict)
+
+                # read the quality bound.
+                q = getkeyvalue(data, "q")
+                if not q in ret_results[domain][problem][k]: ret_results[domain][problem][k][q] = defaultdict(dict)
+                
+                # read the planner.
+                tag = getkeyvalue(data, "tag")
+                if not tag in ret_results[domain][problem]: ret_results[domain][problem][k][q][tag] = defaultdict(dict)
+
+                # read the sat-time
+                ret_results[domain][problem][k][q][tag]["sat-time"] = getkeyvalue(data, "sat-time")
+
+    return ret_results
+
+def dump_list_to_csv(csv_dump, output_file):
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    with open(output_file, "w") as f:
+        for line in csv_dump:
+            f.write(line + "\n")
+    return 0
