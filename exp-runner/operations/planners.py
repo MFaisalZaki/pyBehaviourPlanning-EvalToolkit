ef1def0affe5920112cbb9ef66a191c9f4a93315
MFaisalZaki <mustafa.faisal.eng@gmail.com>
2024-06-04 00:31:19 +0100

    adding plan selection for fi.

--- exp-runner/operations/planners.py
+++ exp-runner/operations/planners.py
@@ -8,8 +8,11 @@ from subprocess import SubprocessError
 
 from unified_planning.engines import PlanGenerationResultStatus as ResultsStatus
 from unified_planning.shortcuts import OneshotPlanner, AnytimePlanner
+from unified_planning.io import PDDLReader
 
+# from diversescore.shortcuts import IBMCalculator
 
+from behaviour_planning.over_domain_models.smt.shortcuts import BehaviourSpace, GoalPredicatesOrdering, MakespanOptimalCostBound, ResourceCount
 
 from .utilities import update_fbi_parameters, generate_summary_file, updatekeyvalue, getkeyvalue, read_planner_cfg
 
@@ -34,9 +37,53 @@ def FIPlannerWrapper(args, task, expdetails):
     def _selection_using_first_k(k, _planlist):
         return _planlist[:k] if len(_planlist) > k else _planlist
     
-    def _selection_maxsum(args, task, k, _planlist):
+    def _selection_maxsum(args, k, _planlist, _tmprun):
         if len(_planlist) <= k: return _planlist
+        
+        with open(args.experiment_file, 'r') as exp_details_file:
+            exp_details = json.load(exp_details_file)
+        
+        domainfile        = getkeyvalue(exp_details, 'domainfile')
+        problemfile       = getkeyvalue(exp_details, 'problemfile')
+        dumpplansetsize   = len(_planlist)
+        selectplansetsize = k
+        metricslist       = ['stability']
+        dump_json_file    = os.path.join(_tmprun, 'selected-plans')
+        assert False, "This is not implemented yet."
         pass
+    
+    def _selection_bspace(_args, _task, k, _planlist):
+        k = 3 # for development now.
+        with open(_args.experiment_file, 'r') as exp_details_file:
+            exp_details = json.load(exp_details_file)
+        exp_details['base-planner-cfg'] = {'k': k }
+        exp_details['bspace-cfg'] = {'quality-bound-factor': 1.0}
+        cfg = update_fbi_parameters(exp_details, exp_details)
+
+        # convert the plan list to a list of SequentialPlan objects.
+        planlist = [PDDLReader().parse_plan_string(_task, p) for p in _planlist]
+
+        # compute upper bound.
+        upper_bound = max([len(p.actions) for p in planlist])
+        cfg['encoder']     = 'seq' 
+        cfg['upper-bound'] = upper_bound
+        cfg['run-plan-validation'] = False
+        cfg['disable-after-goal-state-actions'] = True
+        cfg['is-utility-planning'] = False
+
+        # create the behaviour space.
+        bspace = BehaviourSpace(_task, cfg)
+
+        # now optimise on the behaviour count.
+        covered_behaviours = set()
+        selected_plans = []
+        for i, plan in enumerate(planlist):
+            if len(covered_behaviours) > k: break
+            ret = bspace.plan_behaviour(plan, i)
+            if ret.behaviour in covered_behaviours: continue
+            covered_behaviours.add(ret.behaviour)
+            selected_plans.append(str(ret))
+        return selected_plans
 
 
     cmd  = [sys.executable]
@@ -85,19 +132,14 @@ def FIPlannerWrapper(args, task, expdetails):
             case 'first-k':
                 planlist = _selection_using_first_k(getkeyvalue(expdetails, 'k'), planlist)
             case 'bspace':
-                pass
+                planlist = _selection_bspace(args, task, getkeyvalue(expdetails, 'k'), planlist)
             case 'maxsum':
-                pass
+                planlist = _selection_maxsum(args, getkeyvalue(expdetails, 'k'), planlist, tmprun)
             case _:
                 assert False, f"Unknown selection method: {selection_method}"
 
         planner_params = read_planner_cfg(args.experiment_file)
         results = generate_summary_file(task, expdetails, 'fi', planner_params, planlist, [])
-
-        
-
-
-
 
         return results
 
