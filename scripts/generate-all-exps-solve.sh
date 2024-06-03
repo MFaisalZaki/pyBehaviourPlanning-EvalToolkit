7df64b005d901208d37a8bf1080be20f1cda9b29
MFaisalZaki <mustafa.faisal.eng@gmail.com>
2024-06-03 23:02:59 +0100

    renaming the exp directories.

--- scripts/generate-all-exps-solve.sh
+++ scripts/generate-all-exps-solve.sh
@@ -2,7 +2,12 @@
 
 python3.11 exp-runner/initenv.py
 
-./scripts/generate-solve-exp.sh $(pwd)/all-exps/exp-fbi-behaviour-count     $(pwd)/sandbox-fbi-behaviour-count
-./scripts/generate-solve-exp.sh $(pwd)/all-exps/exp-fi-behaviour-count      $(pwd)/sandbox-fi-behaviour-count
-./scripts/generate-solve-exp.sh $(pwd)/all-exps/exp-symk-behaviour-count    $(pwd)/sandbox-symk-behaviour-count
+mkdir -p sandbox-classical-behaviour-count-exp
+./scripts/generate-solve-exp.sh $(pwd)/all-exps/classical-behaviour-count/exp-fbi-behaviour-count     $(pwd)/sandbox-classical-behaviour-count-exp/fbi
+./scripts/generate-solve-exp.sh $(pwd)/all-exps/classical-behaviour-count/exp-fi-behaviour-count      $(pwd)/sandbox-classical-behaviour-count-exp/fi
+./scripts/generate-solve-exp.sh $(pwd)/all-exps/classical-behaviour-count/exp-symk-behaviour-count    $(pwd)/sandbox-classical-behaviour-count-exp/symk
+
 ./scripts/generate-solve-exp.sh $(pwd)/all-exps/exp-fbi-different-encodings $(pwd)/sandbox-fbi-different-encodings
+
+mkdir -p sandbox-numeric-behaviour-count-exp
+./scripts/generate-solve-exp.sh $(pwd)/all-exps/numeric-behaviour-count/exp-fbi-behaviour-count $(pwd)/sandbox-numeric-behaviour-count-exp/sandbox-fbi-behaviour-count
