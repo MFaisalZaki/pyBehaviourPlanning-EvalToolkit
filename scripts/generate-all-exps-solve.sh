6b85c7a2539248bc09c461f3e86659235f4e754c
MFaisalZaki <mustafa.faisal.eng@gmail.com>
2024-06-14 14:47:54 +0100

    updating exps.

--- scripts/generate-all-exps-solve.sh
+++ scripts/generate-all-exps-solve.sh
@@ -1,19 +1,15 @@
 python3.11 exp-runner/initenv.py
 
-mkdir -p sandbox-classical-behaviour-count-exp
-./scripts/generate-solve-exp.sh $(pwd)/exps/classical-behaviour-count/fbi-symk  $(pwd)/sandbox-classical-behaviour-count-exp/fbi-symk $(pwd)/external-pkgs/classical-domains
-./scripts/generate-solve-exp.sh $(pwd)/exps/classical-behaviour-count/fi        $(pwd)/sandbox-classical-behaviour-count-exp/fi       $(pwd)/external-pkgs/classical-domains
+
+./scripts/generate-solve-exp.sh $(pwd)/exps/classical-behaviour-count $(pwd)/sandbox-classical-behaviour-count-exp $(pwd)/external-pkgs/classical-domains
 
 # list all slurm scripts
-./scripts/collect-slurm-scripts.sh $(pwd)/sandbox-classical-behaviour-count-exp/fbi-symk/slurm-solve-scripts  $(pwd)/sandbox-classical-behaviour-count-exp/fbi-symk-cmds.txt
-./scripts/collect-slurm-scripts.sh $(pwd)/sandbox-classical-behaviour-count-exp/fi/slurm-solve-scripts        $(pwd)/sandbox-classical-behaviour-count-exp/fi-cmds.txt
+./scripts/collect-slurm-scripts.sh $(pwd)/sandbox-classical-behaviour-count-exp/slurm-solve-scripts $(pwd)/sandbox-classical-behaviour-count-exp/fbi-symk-fi-cmds.txt
 
-mkdir -p sandbox-fbi-different-encodings-exp
-./scripts/generate-solve-exp.sh    $(pwd)/exps/different-encodings/fbi $(pwd)/sandbox-fbi-different-encodings-exp $(pwd)/external-pkgs/classical-domains
-./scripts/collect-slurm-scripts.sh $(pwd)/sandbox-fbi-different-encodings-exp/slurm-solve-scripts $(pwd)/sandbox-fbi-different-encodings-exp/different-encodings-cmds.txt
+./scripts/generate-solve-exp.sh    $(pwd)/exps/different-encodings $(pwd)/sandbox-classical-different-encodings-exp $(pwd)/external-pkgs/classical-domains
+./scripts/collect-slurm-scripts.sh $(pwd)/sandbox-classical-different-encodings-exp/slurm-solve-scripts $(pwd)/sandbox-classical-different-encodings-exp/different-encodings-cmds.txt
 
-mkdir -p sandbox-numeric-behaviour-count-exp
-./scripts/generate-solve-exp.sh $(pwd)/exps/numeric-behaviour-count/fbi $(pwd)/sandbox-numeric-behaviour-count-exp/ $(pwd)/external-pkgs/numeric-domains
+./scripts/generate-solve-exp.sh    $(pwd)/exps/numeric-behaviour-count $(pwd)/sandbox-numeric-behaviour-count-exp/ $(pwd)/external-pkgs/numeric-domains
 ./scripts/collect-slurm-scripts.sh $(pwd)/sandbox-numeric-behaviour-count-exp/slurm-solve-scripts $(pwd)/sandbox-numeric-behaviour-count-exp/numeric-cmds.txt
 
 mkdir -p sandbox-oversubscription-behaviour-count-exp
